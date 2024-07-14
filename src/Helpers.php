59fb1f18c590e18cd28ea9fba47077a783131f28
‪khaled reda‬‏ <speedo2003r@gmail.com>
2024-07-14 09:51:25 +0300

    convert_to_english

--- src/Helpers.php
+++ src/Helpers.php
@@ -28,6 +28,20 @@ if (!function_exists('week_days')) {
     }
 }
 
+#convert arabic number to english format - user model
+if (!function_exists('convert_to_english')) {
+    function convert_to_english($string)
+    {
+        $persian = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
+        $arabic = ['٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'];
+
+        $num = range(0, 9);
+        $convertedPersianNums = str_replace($persian, $num, $string);
+        $englishNumbersOnly = str_replace($arabic, $num, $convertedPersianNums);
+
+        return $englishNumbersOnly;
+    }
+}
 if (!function_exists('hours')) {
     function hours($index = -1)
     {
--- src/Modules/Common/Routes/web.php
+++ src/Modules/Common/Routes/web.php
@@ -1,10 +1,4 @@
 <?php
-Route::get('/', 'WebController@index')->name('home');
-Route::get('policy', 'WebController@policy');
-Route::post('subscribe', 'WebController@subscribe')->name('subscribe');
-Route::get('remove_img', 'AdminController@remove_img')->name('remove_img');
-
-Route::get('search_reults/remove/{id?}', 'WebController@search_reults_remove')->name('search_reults.remove');
 
 Route::get('change_locale', function () {
     if (app()->getLocale() == 'ar') {
@@ -15,21 +9,4 @@ Route::get('change_locale', function () {
     return back();
 })->name('change_locale');
 
-Route::resource('common', 'SiteController');
 
-
-Route::get('343242343235454646/{command}/{action?}', function ($command, $action = null) {
-    define('STDIN', fopen("php://stdin", "r"));
-    if ($action) {
-        \Artisan::call("{$command}:{$action}");
-    } else {
-        \Artisan::call("{$command}");
-    }
-    dd(\Artisan::output());
-});
-
-
-Route::get('testic', function () {
-    $res = send_sms("0550379979", "tesst");
-    dd($res);
-});
